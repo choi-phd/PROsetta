53a6e1749fa54c0518074a5ee52bc86a8adfbed2
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2020-04-10 22:54:17 -0500

    update example

--- R/example.R
+++ R/example.R
@@ -3,39 +3,59 @@ NULL
 
 if (FALSE) {
 
-  cfg <- createConfig(
-    response_file = "inst/data-raw/dat_decesd_v2.csv",
-    anchor_file = "inst/data-raw/anchor_decesd.csv",
-    itemmap_file = "inst/data-raw/imap_decesd.csv"
+  fp <- system.file("data-raw", package = "PROsetta")
+  d <- loadData(
+    response  = "dat_DeCESD_v2.csv",
+    itemmap   = "imap_DeCESD.csv",
+    anchor    = "anchor_DeCESD.csv",
+    input_dir = fp
   )
 
-  input_data <- loadData(cfg)
-  freq_table <- runFrequency(cfg)
-  desc_table <- runDescriptive(cfg)
-  classical_table  <- runClassical(cfg)
-  classical_table2 <- runClassical(cfg, omega = TRUE)
+  freq_table <- runFrequency(d)
+  desc_table <- runDescriptive(d)
+  classical_table  <- runClassical(d)
+  classical_table2 <- runClassical(d, omega = TRUE)
 
-  out_CFA <- runCFA(cfg)
+  out_CFA <- runCFA(d)
 
-  summary(out_CFA$combined, fit.measures = TRUE, standardized = TRUE)
+  lavaan::summary(out_CFA$combined, fit.measures = TRUE, standardized = TRUE)
 
-  out_calib <- runCalibration(cfg)
 
-  mirt::coef(out_calib, IRTpars = TRUE, simplify = TRUE)
+
+  ## Item parameter linking to anchor data
+
+  out_link <- runLinking(d, technical = list(NCYCLES = 1000))
+  out_link <- runLinking(d, method = "FIXEDPAR", technical = list(NCYCLES = 1000))
+  out_link$constants
+  out_link$method
+  out_link$ipar_linked
+  out_link$ipar_anchor
+
+  rsss <- runRSSS(d, out_link) # Map raw scores to standardized scores using linked item parameters
+  head(rsss$`2`)
+
+  rsss$`1`$theta_score
+
+
+
+  ## For item parameter examination, does not perform linking
+
+  out_calib <- runCalibration(d)
+  out_calib <- runCalibration(d, technical = list(NCYCLES = 1000))
+  out <- mirt::coef(out_calib, IRTpars = TRUE, simplify = TRUE)
   mirt::itemfit(out_calib, empirical.plot = 1)
   mirt::itemplot(out_calib, item = 1, type = "info")
   mirt::itemfit(out_calib, "S_X2", na.rm = TRUE)
 
-  cfg@linking_method <- "SL"
-  out_calib_free <- runCalibration(cfg)
-  out_calib_free <- runCalibration(cfg, technical = list(NCYCLES = 1000))
 
-  mirt::coef(out_calib_free, IRTpars = TRUE, simplify = TRUE)
 
-  out_equate <- runLinking(cfg, technical = list(NCYCLES = 1000))
-  out_equate$link@constants$SL
+  ## Map raw scores from one scale to another scale
+  out_eqp <- runEquateObserved(d, scaleTo = 1, scaleFrom = 2, type = "equipercentile", smooth = "loglinear")
+  out_eqp$concordance
 
-  out_equate_equiper <- runEquateObserved(cfg, scaleTo = 1, scaleFrom = 2, type = "equipercentile", smooth = "loglinear")
 
-  inputData
+
+  ## Comparing results
+
+
 }
