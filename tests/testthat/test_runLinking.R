789e536534fb1006c31911e04c0d6935fd734169
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2020-08-18 13:02:05 -0500

    add tests

---
+++ tests/testthat/test_runLinking.R
@@ -0,0 +1,36 @@
+library(mirt)
+
+test_that("runLinking", {
+
+  d <- data_asq
+
+  skip_on_cran()
+
+  set.seed(1)
+  calib <- runLinking(d, method = "MM", technical = list(NCYCLES = 1000))
+  expect_equal(prod(calib$constants), -0.1123204, tolerance = 1e-06)
+  expect_equal(sum(calib$ipar_linked), 364.0231, tolerance = 1e-04)
+
+  set.seed(1)
+  calib <- runLinking(d, method = "MS", technical = list(NCYCLES = 1000))
+  expect_equal(prod(calib$constants), -0.1744304, tolerance = 1e-06)
+  expect_equal(sum(calib$ipar_linked), 361.7756, tolerance = 1e-04)
+
+  set.seed(1)
+  calib <- runLinking(d, method = "HB", technical = list(NCYCLES = 1000))
+  expect_equal(prod(calib$constants), -0.175121, tolerance = 1e-06)
+  expect_equal(sum(calib$ipar_linked), 361.1959, tolerance = 1e-04)
+
+  set.seed(1)
+  calib <- runLinking(d, method = "SL", technical = list(NCYCLES = 1000))
+  expect_equal(prod(calib$constants), -0.1620018, tolerance = 1e-06)
+  expect_equal(sum(calib$ipar_linked), 362.1947, tolerance = 1e-04)
+
+  set.seed(1)
+  calib <- runLinking(d, method = "FIXEDPAR", technical = list(NCYCLES = 1000))
+  n_anchor_items <- dim(calib$ipar_anchor)[1]
+  d <- calib$ipar_linked[1:n_anchor_items, ] - calib$ipar_anchor
+  expect_equal(sum(d), 0, tolerance = 1e-08)
+  expect_equal(sum(calib$ipar_linked), 364.7372, tolerance = 1e-04)
+
+})
