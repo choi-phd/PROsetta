5429c39bfba3a6c60a4b06604267fa6c06135f5c
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2020-08-18 18:30:00 -0500

    appendEscore: accept min_score

--- R/core_functions.R
+++ R/core_functions.R
@@ -314,7 +314,7 @@ getRSSS <- function(ipar, theta_grid, is_minscore_0, prior_mean, prior_sd) {
 }
 
 #' @noRd
-appendEscore <- function(score_table, n_scale, item_par_by_scale) {
+appendEscore <- function(score_table, n_scale, item_par_by_scale, min_score) {
 
   for (s in 1:(n_scale + 1)) {
     for (d in 1:(n_scale + 1)) {
