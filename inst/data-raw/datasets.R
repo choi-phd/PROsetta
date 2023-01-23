c003ee1a5fd4b74c093a90f612b0748aefb91abb
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2023-01-23 17:55:43 -0600

    add item models to example data

--- inst/data-raw/datasets.R
+++ inst/data-raw/datasets.R
@@ -5,12 +5,12 @@ usethis::use_data(response_asq, overwrite = TRUE)
 usethis::use_data(itemmap_asq , overwrite = TRUE)
 usethis::use_data(anchor_asq  , overwrite = TRUE)
 
-response_dep <- read.csv("inst/data-raw/dat_decesd_v2.csv", stringsAsFactors = FALSE)
-itemmap_dep  <- read.csv("inst/data-raw/imap_decesd.csv"  , stringsAsFactors = FALSE)
-anchor_dep   <- read.csv("inst/data-raw/anchor_decesd.csv", stringsAsFactors = FALSE)
-usethis::use_data(response_dep, overwrite = TRUE)
-usethis::use_data(itemmap_dep , overwrite = TRUE)
-usethis::use_data(anchor_dep  , overwrite = TRUE)
+response_dep    <- read.csv("inst/data-raw/dat_decesd_v2.csv"  , stringsAsFactors = FALSE)
+itemmap_dep     <- read.csv("inst/data-raw/imap_decesd.csv"    , stringsAsFactors = FALSE)
+anchor_dep      <- read.csv("inst/data-raw/anchor_decesd.csv"  , stringsAsFactors = FALSE)
+usethis::use_data(response_dep   , overwrite = TRUE)
+usethis::use_data(itemmap_dep    , overwrite = TRUE)
+usethis::use_data(anchor_dep     , overwrite = TRUE)
 
 data_asq <-
   loadData(
