5bcc8b048bffb9ddaae36dd95d4d7f7af80d0035
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2023-01-23 15:11:42 -0600

    import cpp

--- R/import.R
+++ R/import.R
@@ -1,5 +1,6 @@
 ## Written by Seung W. Choi (schoi@austin.utexas.edu)
 
+#' @import Rcpp
 #' @import psych
 #' @import mirt
 #' @import plink
@@ -10,4 +11,5 @@
 #' @importFrom graphics hist text points box legend grid lines strheight
 #' @importFrom mvnfast dmvn
 #' @importFrom TestDesign array_p_gr array_p_gpc array_p_m_gr array_p_m_gpc
+#' @useDynLib PROsetta
 NULL
