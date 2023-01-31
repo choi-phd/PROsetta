9bb0e83f8a707b3af71ec83b76aa9b0839b3a48a
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2023-01-30 20:04:25 -0600

    update codetest

--- tests/testthat/test_runCalibration.r
+++ tests/testthat/test_runCalibration.r
@@ -5,44 +5,51 @@ test_that("runCalibration() works", {
   d <- data_asq
 
   skip_on_cran()
-  skip_on_os("mac")
-  skip_on_os("linux")
-  skip_on_os("solaris")
+  skip_on_ci()
+
+  n_cycles <- 1e5
+  tol <- 1e-6
+  test_tol <- 1e-4
 
   # free calibration, used for diagnostics -------------------------------------
   set.seed(1)
-  calib <- runCalibration(d, technical = list(NCYCLES = 1000))
-  ipar <- mirt::coef(calib, simplify = TRUE)$items
-  expect_equal(sum(ipar), -553.4556, tolerance = 1e-04)
+  o <- runCalibration(d, technical = list(NCYCLES = n_cycles), TOL = tol)
+  ipar <- mirt::coef(o, simplify = TRUE)$items
+  expect_equal(sum(ipar), -552.6381, tolerance = test_tol)
 
   # fixed-parameter calibration, used for linking ------------------------------
   set.seed(1)
-  calib <- runCalibration(d, fix_method = "item", technical = list(NCYCLES = 1000))
-  ipar <- mirt::coef(calib, IRTpars = TRUE, simplify = TRUE)$items
+  o <- runCalibration(d, fix_method = "item", technical = list(NCYCLES = n_cycles), TOL = tol)
+  ipar <- mirt::coef(o, IRTpars = TRUE, simplify = TRUE)$items
 
-  ipar_anchor_original <- d@anchor[, c("a", "cb1", "cb2", "cb3", "cb4")]
-  n_anchor_items <- dim(ipar_anchor_original)[1]
-  ipar_diff <- ipar[1:n_anchor_items, ] - ipar_anchor_original
-  expect_equal(sum(ipar_diff), 0, tolerance = 1e-06)
+  # source parameters
+  ipar_anchor_original <- extractAnchorParameters(d, as_AD = FALSE)
+  ipar_diff <- ipar[rownames(ipar_anchor_original), ] - ipar_anchor_original
+  expect_equal(sum(ipar_diff), 0, tolerance = test_tol)
+
+  # new item parameters
+  expect_equal(sum(ipar), 364.7369, tolerance = test_tol)
 
   # free calibration, used for diagnostics -------------------------------------
   # use calibrated projection (without anchoring)
   set.seed(1)
-  calib <- runCalibration(d, dimensions = 2, technical = list(NCYCLES = 1000))
-  ipar <- mirt::coef(calib, simplify = TRUE)$items
-  expect_equal(sum(ipar), -593.1672, tolerance = 1e-04)
+  o <- runCalibration(d, dimensions = 2, technical = list(NCYCLES = n_cycles), TOL = tol)
+  ipar <- mirt::coef(o, simplify = TRUE)$items
+  expect_equal(sum(ipar), -593.5108, tolerance = test_tol)
 
   # fixed-parameter calibration, used for linking ------------------------------
   # use calibrated projection (with anchoring)
   set.seed(1)
-  calib <- runCalibration(d, dimensions = 2, fix_method = "item", technical = list(NCYCLES = 1000))
-  ipar <- mirt::coef(calib, simplify = TRUE)$items
+  o <- runCalibration(d, dimensions = 2, fix_method = "item", technical = list(NCYCLES = n_cycles), TOL = tol)
+  ipar <- mirt::coef(o, simplify = TRUE)$items
 
-  ipar_anchor_original <- d@anchor[, c("a", "cb1", "cb2", "cb3", "cb4")]
-  ipar_anchor_original <- convertABtoAD(ipar_anchor_original)
-  n_anchor_items <- dim(ipar_anchor_original)[1]
-  ipar_diff <- ipar[1:n_anchor_items, c(1, 3:6)] - ipar_anchor_original
-  expect_equal(sum(ipar_diff), 0, tolerance = 1e-06)
+  # source parameters
+  ipar_anchor_original <- extractAnchorParameters(d, as_AD = TRUE)
+  ipar_diff <- ipar[rownames(ipar_anchor_original), -2] - ipar_anchor_original
+  expect_equal(sum(ipar_diff), 0, tolerance = test_tol)
+
+  # new item parameters
+  expect_equal(sum(ipar), -526.6563, tolerance = test_tol)
 
   # ignore_nonconv works -------------------------------------------------------
   set.seed(1)
--- tests/testthat/test_runRSSS.R
+++ tests/testthat/test_runRSSS.R
@@ -1,58 +1,60 @@
 library(mirt)
 
-test_that("runRSSS", {
+test_that("runRSSS() works", {
 
   d <- data_asq
 
   skip_on_cran()
-  skip_on_os("mac")
-  skip_on_os("linux")
-  skip_on_os("solaris")
+  skip_on_ci()
+
+  n_cycles <- 1e5
+  tol <- 1e-6
+  test_tol <- 1e-4
 
   set.seed(1)
-  calib <- runLinking(d, method = "FIXEDPAR", technical = list(NCYCLES = 1000))
+  o <- runLinking(d, method = "FIXEDPAR", technical = list(NCYCLES = n_cycles), TOL = tol)
 
-  score_table <- runRSSS(d, calib)
+  score_table <- runRSSS(d, o)
 
-  expect_equal(sum(score_table$`1`$eap    ** 2), 413.6903, tolerance = 1e-04)
-  expect_equal(sum(score_table$`1`$eap_se ** 2), 3.516712, tolerance = 1e-04)
+  expect_equal(sum(score_table$`1`$eap    ** 2), 413.6903, tolerance = test_tol)
+  expect_equal(sum(score_table$`1`$eap_se ** 2), 3.516712, tolerance = test_tol)
 
-  expect_equal(sum(log(score_table$`1`$escore_1))       , 511.1817, tolerance = 1e-04)
-  expect_equal(sum(log(score_table$`1`$escore_2))       , 382.0980, tolerance = 1e-04)
-  expect_equal(sum(log(score_table$`1`$escore_combined)), 544.7580, tolerance = 1e-04)
+  expect_equal(sum(log(score_table$`1`$escore_1))       , 511.1817, tolerance = test_tol)
+  expect_equal(sum(log(score_table$`1`$escore_2))       , 382.0981, tolerance = test_tol)
+  expect_equal(sum(log(score_table$`1`$escore_combined)), 544.7580, tolerance = test_tol)
 
-  expect_equal(sum(score_table$`2`$eap    ** 2), 224.7138, tolerance = 1e-04)
-  expect_equal(sum(score_table$`2`$eap_se ** 2), 5.638811, tolerance = 1e-04)
+  expect_equal(sum(score_table$`2`$eap    ** 2), 224.7130, tolerance = test_tol)
+  expect_equal(sum(score_table$`2`$eap_se ** 2), 5.638760, tolerance = test_tol)
 
-  expect_equal(sum(log(score_table$`2`$escore_1))       , 200.5240, tolerance = 1e-04)
-  expect_equal(sum(log(score_table$`2`$escore_2))       , 150.7272, tolerance = 1e-04)
-  expect_equal(sum(log(score_table$`2`$escore_combined)), 213.4005, tolerance = 1e-04)
+  expect_equal(sum(log(score_table$`2`$escore_1))       , 200.5239, tolerance = test_tol)
+  expect_equal(sum(log(score_table$`2`$escore_2))       , 150.7272, tolerance = test_tol)
+  expect_equal(sum(log(score_table$`2`$escore_combined)), 213.4005, tolerance = test_tol)
 
-  expect_equal(sum(score_table$`combined`$eap    ** 2), 677.9280, tolerance = 1e-04)
-  expect_equal(sum(score_table$`combined`$eap_se ** 2), 4.451836, tolerance = 1e-04)
+  expect_equal(sum(score_table$`combined`$eap    ** 2), 677.9268, tolerance = test_tol)
+  expect_equal(sum(score_table$`combined`$eap_se ** 2), 4.451822, tolerance = test_tol)
 
-  expect_equal(sum(log(score_table$`combined`$escore_1))       , 708.9261, tolerance = 1e-04)
-  expect_equal(sum(log(score_table$`combined`$escore_2))       , 531.1787, tolerance = 1e-04)
-  expect_equal(sum(log(score_table$`combined`$escore_combined)), 755.0976, tolerance = 1e-04)
+  expect_equal(sum(log(score_table$`combined`$escore_1))       , 708.9261, tolerance = test_tol)
+  expect_equal(sum(log(score_table$`combined`$escore_2))       , 531.1788, tolerance = test_tol)
+  expect_equal(sum(log(score_table$`combined`$escore_combined)), 755.0976, tolerance = test_tol)
 
   set.seed(1)
-  calib <- runLinking(d, method = "CP", technical = list(NCYCLES = 1000))
+  o <- runLinking(d, method = "CP", technical = list(NCYCLES = n_cycles), TOL = tol)
 
-  score_table <- runRSSS(d, calib)
+  score_table <- runRSSS(d, o)
 
-  expect_equal(sum(score_table$`1`$eap_dim1    ** 2), 414.3413, tolerance = 1e-04)
-  expect_equal(sum(score_table$`1`$eap_dim2    ** 2), 371.1619, tolerance = 1e-04)
-  expect_equal(sum(score_table$`1`$eap_se_dim1 ** 2), 3.603925, tolerance = 1e-04)
-  expect_equal(sum(score_table$`1`$eap_se_dim2 ** 2), 18.52101, tolerance = 1e-04)
+  expect_equal(sum(score_table$`1`$eap_dim1    ** 2), 414.3302, tolerance = test_tol)
+  expect_equal(sum(score_table$`1`$eap_dim2    ** 2), 371.4656, tolerance = test_tol)
+  expect_equal(sum(score_table$`1`$eap_se_dim1 ** 2), 3.604095, tolerance = test_tol)
+  expect_equal(sum(score_table$`1`$eap_se_dim2 ** 2), 18.52239, tolerance = test_tol)
 
-  expect_equal(sum(score_table$`2`$eap_dim1    ** 2), 180.4879, tolerance = 1e-04)
-  expect_equal(sum(score_table$`2`$eap_dim2    ** 2), 220.4561, tolerance = 1e-04)
-  expect_equal(sum(score_table$`2`$eap_se_dim1 ** 2), 10.73108, tolerance = 1e-04)
-  expect_equal(sum(score_table$`2`$eap_se_dim2 ** 2), 4.724061, tolerance = 1e-04)
+  expect_equal(sum(score_table$`2`$eap_dim1    ** 2), 180.4573, tolerance = test_tol)
+  expect_equal(sum(score_table$`2`$eap_dim2    ** 2), 220.6060, tolerance = test_tol)
+  expect_equal(sum(score_table$`2`$eap_se_dim1 ** 2), 10.73075, tolerance = test_tol)
+  expect_equal(sum(score_table$`2`$eap_se_dim2 ** 2), 4.724066, tolerance = test_tol)
 
-  expect_equal(sum(score_table$`combined`$eap_dim1    ** 2), 670.684 , tolerance = 1e-04)
-  expect_equal(sum(score_table$`combined`$eap_dim2    ** 2), 652.5277, tolerance = 1e-04)
-  expect_equal(sum(score_table$`combined`$eap_se_dim1 ** 2), 5.859263, tolerance = 1e-04)
-  expect_equal(sum(score_table$`combined`$eap_se_dim2 ** 2), 15.94169, tolerance = 1e-04)
+  expect_equal(sum(score_table$`combined`$eap_dim1    ** 2), 670.6871, tolerance = test_tol)
+  expect_equal(sum(score_table$`combined`$eap_dim2    ** 2), 653.0106, tolerance = test_tol)
+  expect_equal(sum(score_table$`combined`$eap_se_dim1 ** 2), 5.859618, tolerance = test_tol)
+  expect_equal(sum(score_table$`combined`$eap_se_dim2 ** 2), 15.94244, tolerance = test_tol)
 
 })
