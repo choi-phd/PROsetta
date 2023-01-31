69b2c8b0c6b25a838fbf3addbe633b59abadaf71
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2023-01-30 20:04:25 -0600

    update codetest

--- tests/testthat/test_runLinking.R
+++ tests/testthat/test_runLinking.R
@@ -1,48 +1,51 @@
 library(mirt)
 
-test_that("runLinking", {
+test_that("runLinking() works", {
 
   d <- data_asq
 
   skip_on_cran()
-  skip_on_os("mac")
-  skip_on_os("linux")
-  skip_on_os("solaris")
+  skip_on_ci()
 
-  # Hard-coded values are from Windows
-
-  set.seed(1)
-  calib <- runLinking(d, method = "MM", technical = list(NCYCLES = 1000))
-  expect_equal(prod(calib$constants), -0.1123204, tolerance = 1e-03)
-  expect_equal(sum(calib$ipar_linked), 364.0231, tolerance = 1e-03)
+  n_cycles <- 1e5
+  tol <- 1e-6
+  test_tol <- 1e-4
 
   set.seed(1)
-  calib <- runLinking(d, method = "MS", technical = list(NCYCLES = 1000))
-  expect_equal(prod(calib$constants), -0.1744304, tolerance = 1e-03)
-  expect_equal(sum(calib$ipar_linked), 361.7756, tolerance = 1e-03)
+  o <- runLinking(d, method = "MM", technical = list(NCYCLES = n_cycles), TOL = tol)
+  expect_equal(unname(o$constants)[1], 1.007857, tolerance = test_tol)
+  expect_equal(unname(o$constants)[2], -0.109299, tolerance = test_tol)
+  expect_equal(sum(o$ipar_linked), 364.0088, tolerance = test_tol)
 
   set.seed(1)
-  calib <- runLinking(d, method = "HB", technical = list(NCYCLES = 1000))
-  expect_equal(prod(calib$constants), -0.175121, tolerance = 1e-03)
-  expect_equal(sum(calib$ipar_linked), 361.1959, tolerance = 1e-03)
+  o <- runLinking(d, method = "MS", technical = list(NCYCLES = n_cycles), TOL = tol)
+  expect_equal(unname(o$constants)[1], 1.044384, tolerance = test_tol)
+  expect_equal(unname(o$constants)[2], -0.165325, tolerance = test_tol)
+  expect_equal(sum(o$ipar_linked), 361.7414, tolerance = test_tol)
 
   set.seed(1)
-  calib <- runLinking(d, method = "SL", technical = list(NCYCLES = 1000))
-  expect_equal(prod(calib$constants), -0.1620018, tolerance = 1e-03)
-  expect_equal(sum(calib$ipar_linked), 362.1947, tolerance = 1e-03)
+  o <- runLinking(d, method = "HB", technical = list(NCYCLES = n_cycles), TOL = tol)
+  expect_equal(unname(o$constants)[1], 1.042146, tolerance = test_tol)
+  expect_equal(unname(o$constants)[2], -0.166319, tolerance = test_tol)
+  expect_equal(sum(o$ipar_linked), 361.165, tolerance = test_tol)
 
   set.seed(1)
-  calib <- runLinking(d, method = "FIXEDPAR", technical = list(NCYCLES = 1000))
-  n_anchor_items <- dim(calib$ipar_anchor)[1]
-  par_diff <- calib$ipar_linked[1:n_anchor_items, ] - calib$ipar_anchor
-  expect_equal(sum(par_diff), 0, tolerance = 1e-08)
-  expect_equal(sum(calib$ipar_linked), 364.7372, tolerance = 1e-03)
+  o <- runLinking(d, method = "SL", technical = list(NCYCLES = n_cycles), TOL = tol)
+  expect_equal(prod(o$constants), -0.1601742, tolerance = test_tol)
+  expect_equal(sum(o$ipar_linked), 362.1613, tolerance = test_tol)
 
   set.seed(1)
-  calib <- runLinking(d, method = "CP", technical = list(NCYCLES = 1000))
-  n_anchor_items <- dim(calib$ipar_anchor)[1]
-  par_diff <- calib$ipar_linked[1:n_anchor_items, -2] - calib$ipar_anchor
-  expect_equal(sum(par_diff), 0, tolerance = 1e-08)
-  expect_equal(sum(calib$ipar_linked), -526.696, tolerance = 1e-03)
+  o <- runLinking(d, method = "FIXEDPAR", technical = list(NCYCLES = n_cycles), TOL = tol)
+  n_anchor_items <- dim(o$ipar_anchor)[1]
+  ipar_diff <- o$ipar_linked[1:n_anchor_items, ] - o$ipar_anchor
+  expect_equal(sum(ipar_diff), 0, tolerance = test_tol)
+  expect_equal(sum(o$ipar_linked), 364.7369, tolerance = test_tol)
+
+  set.seed(1)
+  o <- runLinking(d, method = "CP", technical = list(NCYCLES = n_cycles), TOL = tol)
+  n_anchor_items <- dim(o$ipar_anchor)[1]
+  ipar_diff <- o$ipar_linked[1:n_anchor_items, -2] - o$ipar_anchor
+  expect_equal(sum(ipar_diff), 0, tolerance = test_tol)
+  expect_equal(sum(o$ipar_linked), -526.6563, tolerance = test_tol)
 
 })
