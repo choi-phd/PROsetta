3e1ddc90092cd7316e706355f425901d3e6fb94a
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2024-02-13 19:39:55 -0600

    add codetests

--- tests/testthat/test_loadData.r
+++ tests/testthat/test_loadData.r
@@ -10,6 +10,46 @@ test_that("loadData() works", {
   expect_true(d@person_id == "prosettaid")
   expect_true(d@scale_id  == "instrument")
 
-  # TODO: add more codetests
+})
+
+test_that("loadData() catches item_model inconsistency between anchor and itemmap", {
+
+  itemmap_malformed <- itemmap_asq
+  itemmap_malformed$item_model[1] <- "GPC"
+
+  expect_error({
+    d <- loadData(
+      response = response_asq,
+      itemmap  = itemmap_malformed,
+      anchor   = anchor_asq
+    )
+  }, "has different models"
+  )
+
+  anchor_malformed <- anchor_asq
+  anchor_malformed$item_model[1] <- "GPC"
+
+  expect_error({
+    d <- loadData(
+      response = response_asq,
+      itemmap  = itemmap_asq,
+      anchor   = anchor_malformed
+    )
+  }, "has different models"
+  )
+
+  itemmap_malformed <- itemmap_asq
+  itemmap_malformed$item_model[1] <- "GPC"
+  anchor_malformed <- anchor_asq
+  anchor_malformed$item_model[2] <- "GPC"
+
+  expect_error({
+    d <- loadData(
+      response = response_asq,
+      itemmap  = itemmap_malformed,
+      anchor   = anchor_malformed
+    )
+  }, "has different models"
+  )
 
 })
